8fca6255eef6e95a20d84f9c222bca9006c2184c
tomee05 <yshigeto@student.42tokyo.jp>
2026-02-08 11:53:15 +0900

    Add ft_print_comb2 function for digit combination
    Implement function to print all combinations of two digits.

---
+++ git_c00/ex06/ft_print_comb2.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void ft_print_comb2(void)
+{
+  int i,j,k,l;
+  i=-1;
+  while (++i<=9)
+  {
+    j=i;
+    while (++j<=8)
+      {
+        write(1, &i, 1);
+        write(1, &j, 1);
+        write(1, " ", 1);
+      }
+  }
+
+  k=-1;
+  while (++k<=9)
+  {
+    l=k;
+    while(++l<=9)
+      {
+        write(1, &k, 1);
+        write(1, &l, 1);
+        if (!(i == 98 && j == 99))
+        	{
+        		write(1, ", ", 2);
+        	}
+      }
+  }
