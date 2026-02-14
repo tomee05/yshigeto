ad7bd6e297f83e9e37b253326d9d1a55790c7841
tomee05 <yshigeto@student.42tokyo.jp>
2026-02-14 18:51:38 +0900

    Add create_grid function for generating grid patterns
    Implement the create_grid function to generate a 4x4 grid based on given parameters and validate it against constraints.

---
+++ rush01/create_grid.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_grid.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkasuya <rkasuya@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 03:05:05 by rkasuya           #+#    #+#             */
+/*   Updated: 2026/02/14 15:18:46 by rkasuya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+// プロトタイプ宣言
+int	**create_x(void);
+int	check_grid(int **grid, int param[4][4]);
+int	check_lr(int array[], int left, int right);
+
+
+int	**create_grid(int param[4][4])
+{
+	int	i;
+	int	**answer;　　//最終的な4*4グリッドの答え
+	int	**colarray;　//1〜4の数字を並べた24通り(4!=24)のパターン
+
+	colarray = create_x();　//create_x()で作った24通りの列パターンをcolarrayに代入
+  
+	i = 0;
+	answer = (int **)malloc(4 * sizeof(int *));　//ansｗerに
+  
+	while (i < 24 * 24 * 24 * 24)　//1列のパターンが4^4(4!)なので、1列で24通り。それらが4行あるので全部で24^4、そのすべてを検証する
+	{
+		answer[0] = colarray[(i / (24 * 24 * 24)) % 24]; //1列目のパターン生成
+		if (check_lr(answer[0], param[2][0], param[3][0]))　//chack_lr(1行目のデータ、左からの正解、右からの正解)
+			i = ((i / (24 * 24 * 24)) + 1) * 24 * 24 * 24;
+  
+		answer[1] = colarray[(i / (24 * 24) % 24)];     //2列目のパターン生成
+		if (check_lr(answer[1], param[2][1], param[3][1]))　//chack_lr(2行目のデータ、左からの正解、右からの正解)
+			i = ((i / (24 * 24)) + 1) * 24 * 24;
+  
+		answer[2] = colarray[(i / 24) % 24];           //3列目のパターン生成
+		if (check_lr(answer[2], param[2][2], param[3][2]))　//chack_lr(3行目のデータ、左からの正解、右からの正解)
+			i = ((i / 24) + 1) * 24;
+  
+		answer[3] = colarray[i % 24];       　　　     //4列目のパターン生成
+		if (check_grid(answer, param))
+			return (answer);
+		i++;
+	}
+	return (NULL);
+}
