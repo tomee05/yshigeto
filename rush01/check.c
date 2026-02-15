dc7aa9f44642d9bfdfbff8888e40d9a2df9a651c
tomee05 <yshigeto@student.42tokyo.jp>
2026-02-15 16:39:14 +0900

    Refactor check functions with comments in Japanese

--- rush01/check.c
+++ rush01/check.c
@@ -11,60 +11,61 @@
 /* ************************************************************************** */
 
 // #include <stdio.h>
-
-int	check(int array[], int visible_count)
+//配列の要素とmaxを一つずつ比べて確認する
+int	check(int array[], int visible_count)　//作った配列にて見える箱の数が期待値と一致するか、左からの確認
 {
-	int	i;
-	int	count;
-	int	max;
+	int	i;　//繰り返し処理用の数
+	int	count;　//見える箱の数
+	int	max;　//最大の高さ
 
 	max = 0;
 	i = 0;
 	count = 0;
-	while (i < 4)
+	
+	while (i < 4)　//配列に入っている4個分繰り返す
 	{
-		if (array[i] > max)
+		if (array[i] > max)　//最大値よりもarray[i]が大きい場合
 		{
-			max = array[i];
-			count++;
+			max = array[i];　//maxにarray[i]を代入
+			count++;　//左から見えている箱の数
 		}
 		i++;
 	}
-	if (count == visible_count)
-		return (1);
-	return (0);
+	if (count == visible_count)　//countと与えられた値が等しいかどうか
+		return (1);　//等しければ、'1'を返す
+	return (0);　//等しくなければ、'0'を返す
 }
 
-int	check_rev(int array[], int visible_count)
+int	check_rev(int array[], int visible_count)　//右からの確認
 {
-	int	i;
-	int	count;
-	int	max;
+	int	i; //繰り返し処理用の数”
+	int	count;　//見える箱の数”
+	int	max;　//最大の高さ”
 
 	max = 0;
-	i = 3;
+	i = 3; //右からなので、4個目から引いていく
 	count = 0;
-	while (i >= 0)
+	while (i >= 0)　//配列に入っている4個分繰り返す
 	{
-		if (array[i] > max)
+		if (array[i] > max)　//最大値よりもarray[i]が大きい場合”
 		{
-			max = array[i];
-			count++;
+			max = array[i];　//maxにarray[i]を代入”
+			count++;　//右から見えている箱の数
 		}
 		i--;
 	}
-	if (count == visible_count)
-		return (1);
-	return (0);
+	if (count == visible_count)　//countと与えられた値が等しいかどうか”
+		return (1);　//等しければ、'1'を返す”
+	return (0);　//等しくなければ、'0'を返す”
 }
 
-int	check_lr(int array[], int left, int right)
+int	check_lr(int array[], int left, int right)　//左右それぞれの値を比較
 {
-	if (check(array, left))
+	if (check(array, left))　//左からの確認→合っていれば'0'を返す
 		return (0);
-	if (check_rev(array, right))
+	if (check_rev(array, right)) //右からの確認→合っていれば'0'を返す
 		return (0);
-	return (1);
+	return (1); //どちらかが合っていなければ'1'を返す
 }
 
 // int	main(void)
